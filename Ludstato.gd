2092af86fdd4a043d54d5eea311975d5b1f25424
hsn6 <hsn6@openmailbox.org>
2017-07-30 10:58:43 +0430

    pafi kaj mortigi

--- Ludstato.gd
+++ Ludstato.gd
@@ -1,7 +1,5 @@
 extends Node
 
-var mia_ludanto_pretita = false
-
 var Ludanto = preload("res://Ludanto.tscn")
 
 var ludanto_nomo = ""
@@ -11,17 +9,16 @@ var Ludantoj = {}
 func _ready():
 #	get_tree().connect("network_peer_connected", self, "_player_connected")
 #	get_tree().connect("network_peer_disconnected", self,"_player_disconnected")
-	get_tree().connect("connected_to_server", self, "_connected_ok")
+	get_tree().connect("connected_to_server", self, "_je_konektita_al_servilo")
 #	get_tree().connect("connection_failed", self, "_connected_fail")
 #	get_tree().connect("server_disconnected", self, "_server_disconnected")
 #	mia_Ludanto = get_node("/root/Radiko/"+str(get_tree().get_network_unique_id()))
 	set_fixed_process(true)
 
-func _fixed_process(delta):
-	if mia_ludanto_pretita:
-		if Input.is_action_pressed("iri"):
-			pass
-#			mia_Ludanto.set_position(mia_Ludanto.get_position()+Vector2(50,50))
+func malplena_loko():
+	var loko = get_node("/root/Radiko/Testilo").get_position()
+	get_node("/root/Radiko/Testilo").set_position(Vector2(0,0))
+	return loko
 
 func servi(nomo):
 	var reto = NetworkedMultiplayerENet.new()
@@ -37,40 +34,54 @@ func aligxi(nomo):
 	ludanto_nomo = nomo
 	get_node("/root/Radiko/Kliento").queue_free()
 
-func _connected_ok():
-	# diru al servilo ke aldonu min al Ludantoj
-	rpc_id(1, "aldoni_Ludanton", get_tree().get_network_unique_id(), ludanto_nomo)
-	#	emit_signal("connection_succeeded")
+# je ludanto
+func _je_konektita_al_servilo():
+	rpc_id(1, "je_konektita_al_servilo", get_tree().get_network_unique_id(), ludanto_nomo)
 
-func malplena_loko():
-	var loko = get_node("/root/Radiko/Testilo").get_position()
-	get_node("/root/Radiko/Testilo").set_position(Vector2(0,0))
-	return loko
-
+# PORFARI kotrolu la procezon
 # por servilo
-remote func aldoni_Ludanton(id, nomo):
-	Ludantoj[nomo] = {"id":id,
-	 "loko":malplena_loko(), "turno":0
-	}
-	aldoni_la_ludantojn()
-	# diru al cxiuj ke prenu Ludantojn
-	rpc("preni_Ludantojn", Ludantoj)
+remote func je_konektita_al_servilo(id, nomo):
+	# se mi estas nova ludanto
+	if not (nomo in Ludantoj.keys()):
+		Ludantoj[nomo] = {
+			"id":id, "loko":malplena_loko(), "turno":0, "vivo":20
+		}
+		aldoni_la_ludantojn()
+		# diru al konektitaj ludantoj ke aldonu min al sia Ludantoj
+		rpc("postglui_novan_ludanton", nomo, Ludantoj[nomo])
+	else:
+		# gxisdatigi mian na id je servilo
+		Ludantoj[nomo]["id"] = id
+		# gxisdatigi mian na id je klientoj
+		rpc("gxisdatigi_na_id", nomo, id)
+	rpc_id(id, "preni_Ludantojn", Ludantoj)
 
-# por ludantoj
-remote func preni_Ludantojn(nova_Ludantoj):
-	Ludantoj = nova_Ludantoj
-	print(Ludantoj)
+# por aliaj ludantoj
+remote func postglui_novan_ludanton(nomo, Ludanto):
+	if get_tree().get_network_unique_id() != Ludanto["id"]:
+		Ludantoj[nomo] = Ludanto
+		aldoni_la_ludantojn()
+
+# por aliaj ludantoj
+remote func gxisdatigi_na_id(nomo, id):
+	if get_tree().get_network_unique_id() != id:
+		Ludantoj[nomo]["id"] = id
+
+# por jxusa konektita ludanto
+remote func preni_Ludantojn(servila_Ludantoj):
+	Ludantoj = servila_Ludantoj
 	aldoni_la_ludantojn()
 
-# post preni_Ludantojn
+# post sxangxigxado de Ludantoj
 func aldoni_la_ludantojn():
 	#aldoni ludantojn se ili ne antauxe aldonigxis
 	for ludanto_nomo in Ludantoj.keys():
-		if not get_node("/root/Radiko").has_node(ludanto_nomo):
+		if not get_node("/root/Radiko/Ludantoj").has_node(ludanto_nomo):
 			var _Ludanto = Ludanto.instance()
 			_Ludanto.set_name(ludanto_nomo)
 			_Ludanto.get_node("Id").set_text(ludanto_nomo)
 			_Ludanto.set_network_master(int(Ludantoj[ludanto_nomo]["id"]))
 			_Ludanto.set_position(Ludantoj[ludanto_nomo]["loko"])
 			_Ludanto.set_rotation(Ludantoj[ludanto_nomo]["turno"])
-			get_node("/root/Radiko").add_child(_Ludanto)
+			_Ludanto.get_node("Vivo").set_scale(Vector2(1,Ludstato.Ludantoj[ludanto_nomo]["vivo"]/_Ludanto.komenca_vivo))
+			get_node("/root/Radiko/Ludantoj").add_child(_Ludanto)
